d44d8bda8d7c23297887ba1d4fa9163852918a6f
MBOYIRE <mboyire14@gmail.com>
2019-02-15 17:34:29 +0200

    adding define New in __init__.py

--- app/models/newz.py
+++ app/models/newz.py
@@ -3,3 +3,11 @@ class Newz:
     Newz class to define Newz objects
     '''
     def __init__(self,source,author,title,description,url,urlToImage,publishedAt,content)
+    self.source = source
+    self.author = author
+    self.title = title
+    self.description = description
+    self.url =http://businesswire.sys-con.com/node/4376242
+    self.urlToImage = urlToImage
+    self.publishedAt  = publishedAt
+    self.content = content
