111e99e76cdb2940f74e94d925435b21a47a6553
MBOYIRE <mboyire14@gmail.com>
2019-02-15 18:12:27 +0200

    ctreating object in request.py

--- app/request.py
+++ app/request.py
@@ -16,3 +16,26 @@ def get_news(category):
             newz_results_list = get_news_response['results']
             newz_results = process_results(newz_results_list)
             return newz_results
+def process_results(newz_list):
+    '''
+    Function that processes the newz result and transform them to a list of objects
+    
+    Args:
+       newz_list: A list of dictionaries that contain newz details
+    Returns:
+       newz_results:A list of objects
+    '''
+    newz_results =[]
+    for newz_item in newz_list:
+        source = newz_item.get('name')
+        author = newz_item.get('author')
+        title = newz_item.get('title')
+        description = newz_item.get('description')
+        url = newz_item.get('url')
+        urlToImage = newz_item.get('image')
+        publishedAt = newz_item.get('published')
+        content = newz_item.get('content')
+        if source:
+            newz_object = Newz(source,author,title,description,url,urlToImage,publishedAt,content)
+            newz_results.append(newz_object)
+    return newz_results
