4d2226c3e6c50daf9a7c6d6f0dcdd381981fc40f
MBOYIRE <mboyire14@gmail.com>
2019-02-15 18:17:36 +0200

    api call in views.py

--- app/views.py
+++ app/views.py
@@ -1,14 +1,20 @@
 from flask import render_template
 from app import app
+from .request import get_newz
 #Views
 @app.route('/')
 def index():
     '''
     View root page function that returns the index page and its data
     '''
+    popular_news = get_news('popular')
+    print(popular_news)
     message = 'Welcome to our website'
     title = 'Welcome to our News website'
-    return render_template('index.html', title = title)
+
+# Getting popular news
+
+    return render_template('index.html', title = title, popular = popular_news)
 
 @app.route('/newz/<newz_id>')
 def newz(newz_id):
