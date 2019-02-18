80274c5f6170e500e2e38b05fbff88b2a890b48c
MBOYIRE <mboyire14@gmail.com>
2019-02-18 11:17:23 +0200

    adding source category in views.py

---
+++ app/source_test.py
@@ -0,0 +1,19 @@
+import unittest
+from models import SOURCE
+Source = source.Source
+
+
+class NewzTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Source class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_source = Source(null,"Thehackernews.com")
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,Source))
+if __name__=='__main__':
+    unittest.main()
--- app/views.py
+++ app/views.py
@@ -1,24 +1,27 @@
 from flask import render_template
 from app import app
-from .request import get_news
+from .request import get_news, get source
 #Views
 @app.route('/')
 def index():
     '''
     View root page function that returns the index page and its data
     '''
-    popular_news = get_news('popular')
-    print(popular_news)
+    general_category= get_source('general')
+    sport_category= get_source('sport')
+    business_category = get_source('business')
+    entertainement_category = get_source('entertainement')
+    technology_category = get_source('technology')
     message = 'Welcome to our website'
-    title = 'Welcome to our News website'
+    title = 'NEWS'
 
 # Getting popular news
 
     return render_template('index.html', title = title, popular = popular_news)
 
-@app.route('/newz/<newz_id>')
-def newz(newz_id):
+@app.route('/source/<source_id>')
+def source(source_id):
     '''
-    view newz page function that returns the newz details page and its data.
+    view source page function that returns the nsource details page and its data.
     '''
-    return render_template('newz.html',id=newz_id)
+    return render_template('source.html',id=source_id)
